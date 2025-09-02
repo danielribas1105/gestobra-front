ef62edc5384ad4672591f23f23fd65881c4473d7
daniel.ribas <daniel.ribas@isdbflowtech.com>
2025-09-02 14:07:16 -0300

    feat(update): add prettierrc to format

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,13 +1,13 @@
 import { z } from "zod"
 
 export const UserSchema = z.object({
-  id: z.string().uuid(),
-  name: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
+	id: z.string().uuid(),
+	name: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
 	cpf: z.string(),
-  email: z.string().email("E-mail inválido"),
+	email: z.string().email("E-mail inválido"),
 	telefone: z.number(),
 	status: z.string(),
-	imagemURL: z.string()
+	imagemURL: z.string(),
 })
 
 // Gerar o tipo TypeScript automaticamente
