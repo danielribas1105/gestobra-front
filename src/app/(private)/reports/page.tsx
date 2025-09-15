862996e94196db38ed730e126c33ae5846af5205
daniel.ribas <daniel.ribas@isdbflowtech.com>
2025-09-15 14:47:45 -0300

    feat(reports): add columns chart

--- src/app/(private)/reports/page.tsx
+++ src/app/(private)/reports/page.tsx
@@ -1,3 +1,4 @@
+import ColumnsChart from "@/components/reports/columns-chart"
 import PageLayout from "@/components/ui/page-layout"
 import TitlePage from "@/components/ui/title-page"
 
@@ -5,8 +6,9 @@ export default function ReportsPage() {
 	return (
 		<PageLayout>
 			<section>
-				<div className="flex flex-col">
+				<div className="flex flex-col gap-5">
 					<TitlePage title="Relatórios" />
+					<ColumnsChart/>
 				</div>
 			</section>
 		</PageLayout>
