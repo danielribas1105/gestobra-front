2031f585ab67afa08fe0ca52bb5ec86ec1894604
daniel.ribas <daniel.ribas@isdbflowtech.com>
2025-09-15 16:26:17 -0300

    feat(car): models adjustment

--- src/components/car/car-card.tsx
+++ src/components/car/car-card.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
 import { IconCircleFilled } from "@tabler/icons-react"
-import Car from "@/data/models/Car"
+import { Car } from "@/schemas/car"
 
 export interface CarCardProps {
 	car: Car
@@ -12,14 +12,14 @@ export default function CarCard({ car }: CarCardProps) {
 		<Link href={`/cars/${car.id}`}>
 			<article className="w-56 h-64 border-2 rounded-lg p-2 flex flex-col gap-2">
 				<div className="relative w-full h-36 flex justify-center overflow-hidden">
-					<Image src={car.imagemURL} alt="Foto da veiculo" fill className="object-cover rounded-lg" />
+					<Image src={car.image_url} alt="Foto da veiculo" fill className="object-cover rounded-lg" />
 				</div>
 				<div className="flex flex-col gap-1">
-					<header>{car.nome}</header>
-					<section>Código: {car.id}</section>
+					<header>{car.model}</header>
+					<section>Placa: {car.license}</section>
 					<footer className="flex items-center gap-1">
-						<IconCircleFilled size={16} color={car.status === "ativo" ? "#00FF00" : "#FF0000"} />
-						<span className="text-sm uppercase">{car.status}</span>
+						<IconCircleFilled size={16} color={car.active ? "#00FF00" : "#FF0000"} />
+						<span className="text-sm uppercase">{car.active}</span>
 					</footer>
 				</div>
 			</article>
--- src/components/car/list-cars.tsx
+++ src/components/car/list-cars.tsx
@@ -1,10 +1,11 @@
 import cars from "@/data/constants/Cars"
 import CarCard from "./car-card"
+import { Car } from "@/schemas/car"
 
 export default function ListCars() {
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-			{cars.map((car) => (
+			{cars.map((car: Car) => (
 				<CarCard key={car.id} car={car} />
 			))}
 		</div>
