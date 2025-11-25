462e841f4ba8ca4fbc586c85a9d86b647f3999de
Daniel Ribas <danielribas1105@gmail.com>
2025-11-24 21:17:40 -0300

    add avatar image

--- src/components/user/user-card.tsx
+++ src/components/user/user-card.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link"
 import Image from "next/image"
 import { IconCircleFilled } from "@tabler/icons-react"
-import User from "@/data/models/User"
+import avatar from "@/../public/img-user.png"
+import { User } from "@/schemas/user"
 
 export interface UserCardProps {
 	user: User
@@ -12,7 +13,12 @@ export default function UserCard({ user }: UserCardProps) {
 		<Link href={`/obras/${user.id}`}>
 			<article className="w-56 h-64 border-2 rounded-lg p-2 flex flex-col gap-2">
 				<div className="relative w-full h-36 flex justify-center overflow-hidden">
-					<Image src={user.image_url} alt="Avatar usuário" fill className="object-cover rounded-lg" />
+					<Image
+						src={user.image_url ?? avatar}
+						alt="Avatar usuário"
+						fill
+						className="object-cover rounded-lg"
+					/>
 				</div>
 				<header>{user.name}</header>
 				<section>Código: {user.id}</section>
